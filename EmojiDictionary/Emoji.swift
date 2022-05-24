66d5a0aaa15e7dca638b1d7371b0366aad4c58a7
Julian Worden <julianworden@Julians-MacBook-Air.local>
2022-05-23 17:51:18 -0700

    Initial Commit

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Julian Worden on 5/23/22.
+//
+
+import Foundation
+
+struct Emoji {
+    private(set) var symbol: String
+    private(set) var name: String
+    private(set) var description: String
+    private(set) var usage: String
+}
---
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  EmojiTableViewController.swift
+//  EmojiDictionary
+//
+//  Created by Julian Worden on 5/23/22.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    var emojis: [Emoji] = [
+       Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
+       Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
+       Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
+       Emoji(symbol: "🧑‍💻", name: "Developer", description: "A person working on a MacBook (probably using Xcode to write iOS apps in Swift).", usage: "apps, software, programming"),
+       Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "something slow"),
+       Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
+       Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
+       Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
+       Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
+       Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
+       Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"), Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+       Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion")
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+
+        let emoji = emojis[indexPath.row]
+
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(emoji.symbol) - \(emoji.name)"
+        content.secondaryText = emoji.description
+        cell.contentConfiguration = content
+
+        cell.showsReorderControl = true
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojis[indexPath.row]
+        print("\(emoji.symbol) \(indexPath)")
+    }
+
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedEmoji = emojis.remove(at: sourceIndexPath.row)
+        emojis.insert(movedEmoji, at: destinationIndexPath.row)
+    }
+
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+
+}
